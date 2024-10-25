d5cfca6de206ad5fc0fe6af3a61eac95ca2d80e1
danieloeyss <danielf2721@gmail.com>
2024-10-25 14:35:14 +0700

    test

--- src/assets/components/Footer.tsx
+++ src/assets/components/Footer.tsx
@@ -1,11 +1,12 @@
 const Footer = () => {
   return (
     <div className="bg-[#020617]">
-        <div className="container h-6 flex justify-between">
-    
-        </div>
+      <div className="container h-6 flex justify-between">
+        <div className=""></div>
+        <div></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
